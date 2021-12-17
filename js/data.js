fbc6a1f627ad59c7352b2893f33dc67ec3fa3f4b
kibethh <kibethh@gmail.com>
2021-12-17 09:21:51 +0300

    Init Commit

---
+++ js/data.js
@@ -0,0 +1,21 @@
+const sublinks = [
+  {
+    page: "home",
+    links: [],
+  },
+  {
+    page: "about",
+    links: [],
+  },
+  {
+    page: "Get_In_Touch",
+    links: [],
+  },
+
+  {
+    page: "projects",
+    links: [],
+  },
+];
+
+export default sublinks;
